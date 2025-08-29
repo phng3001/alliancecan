cb1412a89865fa802985c1f0f7a36e452831b469
phng3001 <phng3001@gmail.com>
2025-08-29 17:22:03 -0400

    ncbi-genome-download from nibi

---
+++ ncbi-genome-download/readme.md
@@ -0,0 +1,58 @@
+# Source
+https://github.com/kblin/ncbi-genome-download/
+
+# Function
+Download genomes from the NCBI FTP servers
+
+# Container
+ncbi-genome-download_v0.3.3.sif
+
+# Usage
+## Shell
+### Options
+```bash
+apptainer exec ncbi-genome-download_v0.3.3.sif ncbi-genome-download --help
+```
+### Example
+#### Download all reference genomes of the genus Clostridium
+> Check which genomes to download
+```bash
+apptainer exec \
+--bind /etc/pki/ca-trust/extracted/pem/tls-ca-bundle.pem:/etc/pki/tls/certs/ca-bundle.crt \
+ncbi-genome-download_v0.3.3.sif ncbi-genome-download bacteria \
+--genera Clostridium \
+--refseq-categories reference \
+--dry-run
+```
+> Download these genomes in the fasta format right into the output folder and save genome metadata
+```bash
+apptainer exec \
+--bind /etc/pki/ca-trust/extracted/pem/tls-ca-bundle.pem:/etc/pki/tls/certs/ca-bundle.crt \
+ncbi-genome-download_v0.3.3.sif ncbi-genome-download bacteria \
+--genera Clostridium \
+--refseq-categories reference \
+--formats fasta \
+--flat-output \
+-p 4 \
+-o clostridium_genomes \
+--metadata-table clostridium_metadata \
+--progress-bar
+```
+
+## Scripting
+```bash
+bash run_apptainer_ncbi-genome-download_refseq_bacteria.sh <container> <species_taxid> <format> <output_prefix>
+```
+or
+```bash
+bash run_apptainer_ncbi-genome-download_genbank.sh <container> <species_taxid> <format> <output_prefix>
+```
+### Example
+#### Download fasta format of Streptococcus acidominimus from RefSeq
+```bash
+bash run_apptainer_ncbi-genome-download_bacteria.sh ncbi-genome-download_v0.3.3.sif 1326 fasta streptococcus_acidominimus
+```
+#### Download all available formats of Leishmania guyanensis assemblies from GenBank
+```bash
+bash run_apptainer_ncbi-genome-download_genbank.sh ncbi-genome-download_v0.3.3.sif 5670 all leishmania_guyanensis
+```
---
+++ ncbi-genome-download/run_apptainer_ncbi-genome-download_genbank.sh
@@ -0,0 +1,69 @@
+#!/bin/bash
+#SBATCH --account=def-mouellet
+#SBATCH --time=3:00:00
+#SBATCH --nodes=1
+#SBATCH --cpus-per-task=4
+#SBATCH --mem-per-cpu=4G
+#SBATCH --job-name=run_apptainer_ncbi-genome-download
+
+######### Preprocessing #########
+
+# Check if the correct number of arguments is provided
+if [ "$#" -ne 4 ]; then
+    echo "Usage on terminal: bash $0 <container> <species_taxid> <format> <output_prefix>"
+    echo "Usage on cluster: sbatch $0 <container> <species_taxid> <format> <output_prefix>"
+    exit 1
+fi
+
+# Load modules
+module purge
+module load StdEnv/2023 apptainer/1.3.5
+
+# Export variables
+export OMP_NUM_THREADS=${SLURM_CPUS_PER_TASK:-4}
+
+# Asign arguments to variables
+container="$1"
+species_taxid="$2"
+format="$3"
+output_prefix="$4"
+
+
+
+######### Genome downloading #########
+
+# List assemblies for download
+apptainer exec \
+--bind /etc/pki/ca-trust/extracted/pem/tls-ca-bundle.pem:/etc/pki/tls/certs/ca-bundle.crt \
+$container ncbi-genome-download all \
+--section genbank \
+--species-taxids $species_taxid \
+--dry-run > ${output_prefix}_list.tsv
+# Remove the first line (message)
+sed -i '1d' ${output_prefix}_list.tsv 
+
+# Download assemblies
+apptainer exec \
+--bind /etc/pki/ca-trust/extracted/pem/tls-ca-bundle.pem:/etc/pki/tls/certs/ca-bundle.crt \
+$container ncbi-genome-download all \
+--section genbank \
+--species-taxids $species_taxid \
+-o ${output_prefix}_genomes \
+--formats $format \
+--flat-output \
+--metadata-table ${output_prefix}_metadata.tsv \
+-p $OMP_NUM_THREADS \
+--progress-bar
+# Unzip files
+gunzip ${output_prefix}_genomes/*.gz
+
+
+
+# Save stdout
+if [[ -n "$SLURM_JOB_ID" && "$SLURM_JOB_ID" -ne 0 ]]; then
+    sacct -j $SLURM_JOB_ID --format=JobID%16,Submit,Start,Elapsed,NCPUS,ExitCode,NodeList%8
+    #sacct -j $SLURM_JOB_ID --format=JobID%16,Submit,Start,Elapsed,NCPUS,ExitCode,NodeList%8,MaxRSS    
+    if [[ -f "slurm-${SLURM_JOB_ID}.out" ]]; then
+        mv slurm-${SLURM_JOB_ID}.out run_apptainer_ncbi-genome-download-${SLURM_JOB_ID}.out
+    fi
+fi
