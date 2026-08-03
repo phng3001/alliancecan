1231f5025a9d4ddd62942e591f689f6e784aa2e3
phng3001 <phng3001@gmail.com>
2026-08-03 18:01:10 -0400

    Add run_apptainer_snippy_core.sh

---
+++ snippy/run_apptainer_snippy_core.sh
@@ -0,0 +1,62 @@
+#!/bin/bash
+# P=NP
+#SBATCH --account=def-mouellet
+#SBATCH --time=06:00:00
+#SBATCH --nodes=1
+#SBATCH --cpus-per-task=4
+#SBATCH --mem-per-cpu=4G
+#SBATCH --job-name=run_apptainer_snippy_core
+
+######### Preprocessing #########
+
+# Check if the correct number of arguments is provided
+if [ "$#" -ne 3 ]; then
+    echo "Usage on terminal: bash $0 <container> <reference> <sample_list>"
+    echo "Usage on cluster: sbatch $0 <container> <reference> <sample_list>"
+    exit 1
+fi
+
+# Load modules
+module purge
+module load StdEnv/2023 apptainer/1.3.5
+
+# Export variables
+export OMP_NUM_THREADS=${SLURM_CPUS_PER_TASK:-4}
+# export TMPDIR=${SLURM_TMPDIR:-$HOME/scratch/}
+# I don't know why but the above line causes error while running on compute nodes
+# e.g. "--tmpdir '/localscratch/phng3001.40951974.0' is not a directory"
+export TMPDIR=$HOME/scratch/
+
+# Asign arguments to variables
+container="$1"
+reference="$2"
+sample_list="$3"
+
+
+
+######### Core SNP alignment #########
+
+apptainer exec \
+-W $TMPDIR \
+$container snippy-core \
+--ref $reference \
+$(paste -s -d ' ' $sample_list)
+
+if [ -f "core.full.aln" ]; then
+    apptainer exec \
+    $container snippy-clean_full_aln \
+    core.full.aln > core.full.clean.aln
+else
+    echo "Problem building core SNP alignment, file core.full.aln was not generated."
+fi
+
+
+
+# Save stdout
+if [[ -n "$SLURM_JOB_ID" && "$SLURM_JOB_ID" -ne 0 ]]; then
+    sacct -j $SLURM_JOB_ID --format=JobID%16,Submit,Start,Elapsed,NCPUS,ExitCode,NodeList%8
+    #sacct -j $SLURM_JOB_ID --format=JobID%16,Submit,Start,Elapsed,NCPUS,ExitCode,NodeList%8,MaxRSS    
+    if [[ -f "slurm-${SLURM_JOB_ID}.out" ]]; then
+        mv slurm-${SLURM_JOB_ID}.out run_apptainer_snippy_core-${SLURM_JOB_ID}.out
+    fi
+fi
